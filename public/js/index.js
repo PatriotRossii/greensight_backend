9adb32d8f507e1b8be0edd6c4cb268a9b5205060
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-03 19:01:49 +0300

    Handle responses from signup.php in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,13 @@
 $(function() {
+	var $successAlert = $("#registrationSuccess");
+	var $failAlert = $("#registrationFail");
+	var $form = $("#signup")
+
 	var request;
 	$("#signup").submit(function(event) {
+		$successAlert.hide();
+		$failAlert.hide();
+
 		event.preventDefault();
 
 		if (request) {
@@ -12,7 +19,17 @@ $(function() {
 		var serializedData = $form.serialize();
 
 		$inputs.prop("disabled", true);
-		request = $.post("signup.php", serializedData).fail(function (jqXHR, textStatus, errorThrown) {
+		request = $.post("signup.php", serializedData, function(data) {
+			const response = JSON.parse(data);
+			console.log(response);
+			if (response.status == "success") {
+				$successAlert.show();
+				$form.hide();
+			} else {
+				$failAlert.text(response.message);
+				$failAlert.show();
+			}
+		}).fail(function (jqXHR, textStatus, errorThrown) {
 			console.error("The following error occured: " +
 				errorThrown);
 		}).always(function() {
