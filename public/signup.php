079dc197396d97e5fbc50025727085a05d4d3c85
PatriotRossii <patriotrossii2019@mail.ru>
2023-03-03 19:38:51 +0300

    Encapsulate validation logic into SignupFormValidator class

--- public/signup.php
+++ public/signup.php
@@ -15,56 +15,68 @@ $users = [
 	["id" => 2, "first_name" => "Marieke", "last_name" => "Blessing", "email" => "mar-bl@diaperstack.com"],
 ];
 
-function validateEmail($email)
-{
-	return filter_var($email, FILTER_VALIDATE_EMAIL);
+class SignupFormValidator {
+	private $fields = ["first_name", "last_name", "email", "password", "passwordConfirmation"];
+	private $response = null;
+
+	private function validateEmail($email)
+	{
+		return filter_var($email, FILTER_VALIDATE_EMAIL);
+	}
+
+	private function validatePassword($password, $password_confirmation)
+	{
+		return $password == $password_confirmation;
+	}
+
+	private function getField($data, $field_name)
+	{
+		return isset($data[$field_name]) ? $data[$field_name] : null;
+	}
+
+	public function validate($request) {
+		global $logger, $users;
+
+		$registration_data = array();
+		foreach ($this->fields as $i => $value) {
+			$registration_data[$value] = $this->getField($request, $value);
+		}
+
+		if (in_array(null, $registration_data)) {
+			$this->response = json_encode([
+				"status" => "error", "message" => "Please, fill all required fields"
+			]);
+			return;
+		} 
+
+		$email_column = array_column($users, "email");
+		$found_key = array_search($registration_data["email"], $email_column);
+
+		if (!$this->validateEmail($registration_data["email"])) {
+			$this->response = json_encode([
+				"status" => "error", "message" => "Please, enter valid email"
+			]);
+		} else if (!$this->validatePassword($registration_data["password"], $registration_data["passwordConfirmation"])) {
+			$this->response = json_encode([
+				"status" => "error", "message" => "Passwords do not match, please try again"
+			]);
+		} else if ($found_key !== false) {
+			$logger->info("User with email " . $users[$found_key]["email"] . " was found:", $users[$found_key]);
+			$this->response = json_encode([
+				"status" => "error", "message" => "User with given email already exists"
+			]);
+		} else {
+			$this->response = json_encode([ "status" => "success" ]);
+		}
+	}
+
+	public function response(): string {
+		return $this->response;
+	}
 }
 
-function validatePassword($password, $password_confirmation)
-{
-	return $password == $password_confirmation;
-}
-
-function getField($data, $field_name)
-{
-	return isset($data[$field_name]) ? $data[$field_name] : null;
-}
-
-$fields = ["first_name", "last_name", "email", "password", "passwordConfirmation"];
-$registration_data = array();
-foreach ($fields as $i => $value) {
-	$registration_data[$value] = getField($_POST, $value);
-}
-
-if (in_array(null, $registration_data)) {
-	echo json_encode([
-		"status" => "error", "message" => "Please, fill all required fields"
-	]);
-	exit(1);
-}
-if (!validateEmail($registration_data["email"])) {
-	echo json_encode([
-		"status" => "error", "message" => "Please, enter valid email"
-	]);
-	exit(1);
-}
-if (!validatePassword($registration_data["password"], $registration_data["passwordConfirmation"])) {
-	echo json_encode([
-		"status" => "error", "message" => "Passwords do not match, please try again"
-	]);
-	exit(1);
-}
-
-$email_column = array_column($users, "email");
-$found_key = array_search($registration_data["email"], $email_column);
-if ($found_key !== false) {
-	$logger->info("User with email " . $users[$found_key]["email"] . " was found:", $users[$found_key]);
-	echo json_encode([
-		"status" => "error", "message" => "User with given email already exists"
-	]);
-	exit(1);
-}
-
-echo json_encode([ "status" => "success" ]);
+$validator = new SignupFormValidator();
+$validator->validate($_POST);
+echo $validator->response();
 
 ?>
